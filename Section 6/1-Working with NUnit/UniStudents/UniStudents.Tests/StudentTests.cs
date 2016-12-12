c10998d779677e80545265057dadde6e6b391ee7
amr-swalha <amr.swalha.code@outlook.com>
2016-12-12 19:44:02 +0200

    Adding nunit first code

---
+++ Section 6/1-Working with NUnit/UniStudents/UniStudents.Tests/StudentTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DAL.Models;
+
+namespace UniStudents.Tests
+{
+    [TestFixture]
+    public class StudentTests
+    {
+        Student objStudent = new Student();
+        [Test]
+        public void CheckStudent()
+        {
+            Student student = new Student();
+            student.ID = 1;
+            Assert.AreEqual(student.ID, 1);
+        }
+        [TestCase(6)]
+        public void CheckStudentIdUpdate(int id)
+        {
+
+            Assert.AreEqual(objStudent.ID, id);
+        }
+
+        [SetUp]
+        public void SetupThevalueofstudent()
+        {
+            objStudent.ID = 6;
+        }
+    }
+}

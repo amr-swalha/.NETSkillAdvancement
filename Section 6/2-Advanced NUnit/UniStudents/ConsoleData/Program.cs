ff3a5b13737b3afb551c5f3fbf508e91e50948f9
amr-swalha <amr.swalha.code@outlook.com>
2016-12-17 11:11:36 +0200

    Adding Advanced NUnit Lecture

---
+++ Section 6/2-Advanced NUnit/UniStudents/ConsoleData/Program.cs
@@ -0,0 +1,17 @@
+﻿using BL;
+using DAL.Models;
+
+namespace ConsoleData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentBL student = new StudentBL();
+            Profile profile = new Profile();
+            profile.Email = "test";
+            profile.FullName = "test";
+            student.AddStudent(new Student() { profile = profile});
+        }
+    }
+}
